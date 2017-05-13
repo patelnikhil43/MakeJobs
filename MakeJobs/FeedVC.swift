c351111fa216bd1e268127e60bdc8df84bd648e8
patelnikhil43 <patelnikhil43@gmail.com>
2017-05-13 11:55:42 -0400

    Feed & Post Added with Tab Controller & Firebase

---
+++ MakeJobs/FeedVC.swift
@@ -0,0 +1,39 @@
+//
+//  FeedVC.swift
+//  MakeJobs
+//
+//  Created by Nikhil on 5/13/17.
+//  Copyright © 2017 Nikhil. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+
+    @IBOutlet weak var tableViewBoard: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+      
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
